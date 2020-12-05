273eaa8f6700505cb44896dad561c75f2a04e0f8
Amartya Aishwarya <amartya970914@gmail.com>
2020-12-05 15:03:29 +0530

    adding ele into array ingrediants

--- src/app/shopping-list/shoppping-list.service.ts
+++ src/app/shopping-list/shoppping-list.service.ts
@@ -1,6 +1,19 @@
+import { Ingredient} from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+  ingrediantsChanged = new EventEmitter<Ingredient[]>();
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 
-
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingrediantsChanged.emit(this.ingredients.slice());
+  }
 }
