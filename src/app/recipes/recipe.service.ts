89e771038373f1ea873007b045e6cf177f79de43
Amartya Aishwarya <amartya970914@gmail.com>
2020-12-06 00:28:43 +0530

    recipe service edited

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,26 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
-
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Paneer Chilly',
+      'Spicy Paneer Dish',
+      'https://www.funfoodfrolic.com/wp-content/uploads/2020/04/Chilli-Paneer-Thumbnail.jpg',
+      [new Ingredient('Paneer', 1), new Ingredient('Onion', 2)]
+    ),
+    new Recipe(
+      'Burger',
+      'Veg Burger',
+      'https://i.insider.com/5d8ca1222e22af53447766c2?width=1100&format=jpeg&auto=webp',
+      [new Ingredient('Bun', 2), new Ingredient('Potato', 3)]
+    ),
   ];
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
-
 }
