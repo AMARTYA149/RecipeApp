5bebe2ccd47b9d8d89b5188c6d0e6b30070503f9
Amartya Aishwarya <amartya970914@gmail.com>
2020-12-22 00:20:42 +0530

    routes done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shoppping-list.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
