0bcc445e26ba1c8cc6e588e6e5c6f39bdb70db6a
AMARTYA149 <amartya970914@gmail.com>
2022-11-09 22:08:48 +0530

    update, delete & add recipe

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -6,13 +6,14 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { take } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 import * as fromApp from '../store/app.reducer';
 import * as RecipesActions from '../recipes/store/recipe.actions';
 import { Actions, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
@@ -24,14 +25,32 @@ export class RecipeResolverService implements Resolve<Recipe[]> {
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipesService.getRecipes();
+    // const recipes = this.recipesService.getRecipes();
 
-    if (recipes.length === 0) {
-      // return this.dataStorageService.fetchRecipes();
-      this.store.dispatch(new RecipesActions.FetchRecipes());
-      return this.actions$.pipe(ofType(RecipesActions.SET_RECIPES), take(1));
-    } else {
-      return recipes;
-    }
+    // if (recipes.length === 0) {
+    //   // return this.dataStorageService.fetchRecipes();
+    //   this.store.dispatch(new RecipesActions.FetchRecipes());
+    //   return this.actions$.pipe(ofType(RecipesActions.SET_RECIPES), take(1));
+    // } else {
+    //   return recipes;
+    // }
+
+    return this.store.select('recipes').pipe(
+      take(1),
+      map((recipesState) => {
+        return recipesState.recipes;
+      }),
+      switchMap((recipes) => {
+        if (recipes.length === 0) {
+          this.store.dispatch(new RecipesActions.FetchRecipes());
+          return this.actions$.pipe(
+            ofType(RecipesActions.SET_RECIPES),
+            take(1)
+          );
+        } else {
+          of(recipes);
+        }
+      })
+    );
   }
 }
